a28b8cd0a9d9fcc0f958f1b97b94257be9d836cb
EmNudge <emnudge@gmail.com>
2021-12-28 21:59:13 -0500

    fix solid startup

--- src/components/Solid/Tabs/Tabs.tsx
+++ src/components/Solid/Tabs/Tabs.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource solid-js */
 import { 
     createSignal, createContext, useContext, 
-    onCleanup, onMount, createMemo, For, createEffect,
+    onCleanup, For, batch,
 } from 'solid-js';
 
 const TabsContext = createContext({
@@ -9,25 +9,25 @@ const TabsContext = createContext({
     removeTab: (_label: string) => { console.error('using old removeTab') }
 });
 
-export const TabContent = (props: { label: string, children: unknown }) => {
+export const TabContent = (props: { label: string, children: Element[] }) => {
     const { addTab, removeTab } = useContext(TabsContext);
 
-    onMount(() => addTab(props.label, props.children));
+    addTab(props.label, props.children);
     onCleanup(() => removeTab(props.label));
 
     return <></>;
 };
 
-const TabsContainer = (props: { children: unknown }) => {
+const TabsContainer = (props: { children: Element[] }) => {
     const [tabs, setTabs] = createSignal<{ [label: string]: any }>({});
     const [currentTab, setCurrentTab] = createSignal('');
 
     const providerData = {
-        addTab(label: string, children: unknown) {
-            setTabs(prev => {
-                if (!Object.keys(prev).length) setCurrentTab(label);
-                return { ...prev, [label]: children };
-            });
+        addTab(label: string, children: Element[]) {
+            batch(() => {
+                if (!Object.keys(tabs()).length) setCurrentTab(label);
+                setTabs(prev => ({ ...prev, [label]: children }));
+            })
         },
         removeTab(label: string) {
             setTabs(prev => {
@@ -39,7 +39,6 @@ const TabsContainer = (props: { children: unknown }) => {
 
     const selectedTab = (() => {
         const tab = tabs()[currentTab()];
-        console.log({ tab })
         if (!tab) {
             const keysArr = Reflect.ownKeys(tabs());
             if (keysArr.length) return keysArr[0];
@@ -54,8 +53,8 @@ const TabsContainer = (props: { children: unknown }) => {
                 <For each={Object.keys(tabs())}>
                     {(label) => 
                         <span
-                            className={label === currentTab() ? 'selected' : undefined}
-                            onClick={() => {console.log('hey!'); setCurrentTab(label)}}
+                            classList={{ selected: label === currentTab() }}
+                            onClick={[setCurrentTab, label]}
                         >
                             {label}
                         </span>
